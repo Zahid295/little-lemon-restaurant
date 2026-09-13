f4c065fdb50ce5f8150c29e730cdba2efbdccb7d
Zahid295 <Zahidbhatti074@gmail.com>
2026-09-13 20:27:23 +0100

    Add Cart Model

--- backend/littleLemonAPI/models.py
+++ backend/littleLemonAPI/models.py
@@ -26,3 +26,14 @@ class MenuItem(models.Model):
     def __str__(self):
         return self.title
 
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menuitem = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ('menuitem', 'user')
+
