ee0b05318071427cd07205f8c5eaeb331c321cdd
Zahid295 <Zahidbhatti074@gmail.com>
2026-09-13 20:42:21 +0100

    Add cart API route

--- backend/littleLemonAPI/urls.py
+++ backend/littleLemonAPI/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('menu-items', views.Menu_Items.as_view()),
     path('menu-items/<int:pk>', views.MenuItemDetailView.as_view()),
+    path('cart/menu-items', views.CartView.as_view()),
 ]
