71c560c1051876b14032a210d13906b51530a3bd
Zahid295 <Zahidbhatti074@gmail.com>
2026-09-14 18:23:23 +0100

    Add order routes

--- backend/littleLemonAPI/urls.py
+++ backend/littleLemonAPI/urls.py
@@ -4,4 +4,6 @@ urlpatterns = [
     path('menu-items', views.Menu_Items.as_view()),
     path('menu-items/<int:pk>', views.MenuItemDetailView.as_view()),
     path('cart/menu-items', views.CartView.as_view()),
+    path('orders', views.OrderListCreateView.as_view()),
+    path('orders/<int:orderId>', views.OrderDetailUpdateView.as_view()),
 ]
