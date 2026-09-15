94a33aa23196506ea0a7eab2b39fff660c1dba66
Zahid295 <Zahidbhatti074@gmail.com>
2026-09-16 00:10:22 +0100

    Implement ProtectedRoute to guard authenticated pages using access token

---
+++ src/components/ProjectedRoute.js
@@ -0,0 +1,10 @@
+import { useAuth } from "../context/AuthContext";
+import { Navigate } from "react-router-dom";
+
+export default function ProtectedRoute({ children }) {
+  const { accessToken } = useAuth();
+
+  if (!accessToken) return <Navigate to="/login" replace />;
+
+  return children;
+}
